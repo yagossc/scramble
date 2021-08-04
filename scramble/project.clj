90a17a3b754fd4d2e0498f09a18fb5d7f5381176
Yago Santos <yagossc@gmail.com>
2021-08-04 14:05:52 -0300

    Add task-1 scramble function

--- scramble/project.clj
+++ scramble/project.clj
@@ -1,7 +1,17 @@
 (defproject scramble "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/yagossc/scramble/scramble"
+  :license {:name "MIT LICENSE" :url ""}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns scramble.core})
+  :repl-options {:init-ns scramble.core}
+  :main ^:skip-aot scramble.core
+  :target-path "target/%s"
+  :profiles {:socket {:jvm-opts ["-Dclojure.server.repl={:name \"repl-server\"
+                                               :port 5555
+                                               :address \"0.0.0.0\"
+                                               :accept clojure.core.server/repl
+                                               :bind-err true
+                                               :server-daemon false
+                                               :client-daemon false}"]}
+             :uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- scramble/src/scramble/core.clj
+++ scramble/src/scramble/core.clj
@@ -1,6 +1,27 @@
 (ns scramble.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn occur-in?
+  "Builds a list of boolean values that represent, for each char in
+  INPUT-STRING, if this char is sufficiently present in the frequencies
+  map INPUT-MAP"
+  [input-map input-string]
+  (let [tmp-map (atom input-map)]
+    (for [char input-string]
+      (do (if (get @tmp-map char)
+            (do (swap! tmp-map
+                       (fn [old-map]
+                         (update old-map char dec)))
+                (<= 0 (get @tmp-map char)))
+            false)))))
+
+
+(defn scramble?
+  "Checks if a portion of BASE-STRING can be rearranged to form TARGET-STRING."
+  [base-string target-string]
+  (let [base-frequencies (frequencies base-string)]
+    (empty? (filter false? (occur-in? base-frequencies target-string)))))
+
+
+;; REPL testing
+;; (scramble? "abcdefffffffffff" "abcdefg")
