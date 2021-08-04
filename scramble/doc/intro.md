3a2db419cf6fd2f56843393087a1781903b8d2ef
Yago Santos <yagossc@gmail.com>
2021-08-04 13:58:38 -0300

    Add initial lein project structure

---
+++ scramble/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to scramble
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ scramble/test/scramble/core_test.clj
@@ -0,0 +1,7 @@
+(ns scramble.core-test
+  (:require [clojure.test :refer :all]
+            [scramble.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
